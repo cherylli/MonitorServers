96ca951205f0abf26c54c079163f99144b0436a6
Cheryl Murphy <webmaster@cherylli.com>
2020-07-04 00:00:37 +1000

    Update CheckServer.py

--- CheckServer.py
+++ CheckServer.py
@@ -6,7 +6,6 @@ import pickle
 import subprocess
 import platform
 
-from gmail import email_alert
 
 class Server():
     def __init__(self, name, port, connection, priority):
@@ -26,71 +25,59 @@ class Server():
         try:
             if self.connection == "plain":
                 socket.create_connection((self.name, self.port), timeout=10)
-                msg = f"{self.name} is up. On port {self.port} with {self.connection}"
-                success = True
-                self.alert = False
             elif self.connection == "ssl":
-                ssl.wrap_socket(socket.create_connection((self.name, self.port), timeout=10))
-                msg = f"{self.name} is up. On port {self.port} with {self.connection}"
-                success = True
-                self.alert = False
+                ssl.wrap_socket(socket.create_connection(
+                    (self.name, self.port), timeout=10))
             else:
-                if self.ping():
-                    msg = f"{self.name} is up. On port {self.port} with {self.connection}"
-                    success = True
-                    self.alert = False
+                if not self.ping():
+                    raise ConnectionError("Cannot reach server with ping.")
+            msg = f"{self.name} is up. On port {self.port} with {self.connection}"
+            success = True
+            self.alert = False
         except socket.timeout:
-            msg = f"server: {self.name} timeout. On port {self.port}"
-        except (ConnectionRefusedError, ConnectionResetError) as e:
+            msg = f"server: {self.name} is down. On port {self.port}"
+        except (ConnectionRefusedError, ConnectionResetError, ConnectionError) as e:
             msg = f"server: {self.name} {e}"
         except Exception as e:
             msg = f"No Clue??: {e}"
 
-        
-        if success == False and self.alert == False:
-            # Send Alert
-            self.alert = True
-            # Uncomment this once you have setup gmail alerts!
-            # Check out video if you need help!
-            # https://youtu.be/B1IsCbXp0uE
-            # email_alert(self.name,f"{msg}\n{now}","jake@claritycoders.com")
-
-        self.create_history(msg,success,now)
+        self.create_history(msg, success, now)
 
     def create_history(self, msg, success, now):
         history_max = 100
-        self.history.append((msg,success,now))
+        self.history.append((msg, success, now))
 
         while len(self.history) > history_max:
             self.history.pop(0)
 
     def ping(self):
         try:
-            output = subprocess.check_output("ping -{} 1 {}".format('n' if platform.system().lower(
-            ) == "windows" else 'c', self.name ), shell=True, universal_newlines=True)
+            output = subprocess.check_output(
+                f"ping -{'n' if platform.system().lower()=='windows' else 'c'} 1 {self.name}",
+                shell=True,
+                universal_newlines=True)
             if 'unreachable' in output:
                 return False
             else:
                 return True
         except Exception:
-                return False
+            return False
 
 
 if __name__ == "__main__":
     try:
         servers = pickle.load(open("servers.pickle", "rb"))
     except:
-        servers = [ 
+        servers = [
             Server("reddit.com", 80, "plain", "high"),
             Server("msn.com", 80, "plain", "high"),
             Server("smtp.gmail.com", 465, "ssl", "high"),
-            Server("192.168.1.164", 80, "ping", "high"),
-            Server("yahoo.com", 80, "plain", "high")
+            Server("192.168.1.12", 80, "ping", "high"),
         ]
 
     for server in servers:
         server.check_connection()
-        print(len(server.history))
         print(server.history[-1])
+        # print(len(server.history))
 
     pickle.dump(servers, open("servers.pickle", "wb"))
